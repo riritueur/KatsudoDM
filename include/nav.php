f7f1299ebf1b8146c358f963369ef88e44d054a5
Richard Peres <riritueur@gmail.com>
2018-01-03 17:46:12 +0100

    francisé

--- include/nav.php
+++ include/nav.php
@@ -7,7 +7,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-    <a class="navbar-brand" href="index.php">SB Admin v2.0</a>
+    <a class="navbar-brand" href="index.php">Katsudo DM</a>
   </div>
   <!-- /.navbar-header -->
 
@@ -18,7 +18,7 @@
                         <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                     </a>
       <ul class="dropdown-menu dropdown-user">
-        <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+        <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Se déconnecter</a>
         </li>
       </ul>
       <!-- /.dropdown-user -->
@@ -31,13 +31,22 @@
     <div class="sidebar-nav navbar-collapse">
       <ul class="nav" id="side-menu">
         <li>
-          <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+          <a href="index.php"><i class="fa fa-dashboard fa-fw"></i> Tableau de bord</a>
         </li>
         <li>
-          <a href="client.php"><i class="fa fa-edit fa-fw"></i> Clients</a>
+          <a href="client.php"><i class="fa fa-user fa-fw"></i> Clients</a>
         </li>
         <li>
-          <a href="employe.php"><i class="fa fa-table fa-fw"></i> Employés</a>
+          <a href="employe.php"><i class="fa fa-users fa-fw"></i> Employés</a>
+        </li>
+        <li>
+          <a href="fournisseur.php"><i class="fa fa-user fa-fw"></i> Fournisseurs</a>
+        </li>
+        <li>
+          <a href="produit.php"><i class="fa fa-shopping-cart fa-fw"></i> Produits</a>
+        </li>
+        <li>
+          <a href="taxe.php"><i class="fa fa-usd fa-fw"></i> Taxes</a>
         </li>
         <li>
           <a href="#"><i class="fa fa-wrench fa-fw"></i> Factures<span class="fa arrow"></span></a>
@@ -51,16 +60,7 @@
           </ul>
         </li>
         <li>
-          <a href="fournisseur.php"><i class="fa fa-table fa-fw"></i> Fournisseurs</a>
-        </li>
-        <li>
-          <a href="produit.php"><i class="fa fa-table fa-fw"></i> Produits</a>
-        </li>
-        <li>
-          <a href="taxe.php"><i class="fa fa-table fa-fw"></i> Taxes</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+          <a href="#"><i class="fa fa-wrench fa-fw"></i> Test<span class="fa arrow"></span></a>
           <ul class="nav nav-second-level">
             <li>
               <a href="panels-wells.php">Panels and Wells</a>
