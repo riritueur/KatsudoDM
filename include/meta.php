185c99074a553d888fdef748076ac3b42eab5edc
Richard Peres <riritueur@gmail.com>
2018-01-03 02:07:30 +0100

    icon

--- include/meta.php
+++ include/meta.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/8/84/DM_logo.svg" />
+<link rel="shortcut icon" href="ressource/image/logo.svg" />
 
 
 <meta name="author" content="Richard Peres & François RIGAUT" />
@@ -9,10 +9,10 @@
 <meta property="og:site_name" content="KastudoDM" />
 <meta property="og:url" content="http://katsudodm.richard-peres.xyz" />
 <meta property="og:title" content="KastudoDM" />
-<meta property="og:image" content="https://upload.wikimedia.org/wikipedia/commons/8/84/DM_logo.svg" />
+<meta property="og:image" content="http://www.katsudodm.richard-peres.xyz/ressource/image/logo.svg" />
 <meta property="og:description" content="KatsudoDM est un site de d'hébergement en ligne et de nom domaine totalement gratuit !" />
 
-<meta name="twitter:image" content="https://upload.wikimedia.org/wikipedia/commons/8/84/DM_logo.svg" />
+<meta name="twitter:image" content="http://www.katsudodm.richard-peres.xyz/ressource/image/logo.svg" />
 <meta name="twitter:card" content="summary" />
 <meta name="twitter:title" content="KastudoDM" />
 <meta name="twitter:description" content="KatsudoDM est un site de d'hébergement en ligne et de nom domaine totalement gratuit !" />
