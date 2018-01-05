2bd52b9b3fc42e95529201564c62cdf9a14d52f5
Richard Peres <riritueur@gmail.com>
2018-01-06 00:51:06 +0100

    del taxe

--- pages/taxe.php
+++ pages/taxe.php
@@ -73,82 +73,81 @@
                   </thead>
                   <tbody>
                     <?php
-											$result = $bdd->query("SELECT * FROM Taxe");
-											while($data = $result->fetch()){
-												echo '<tr>
-															<td>'.$data['type_t'].'</td>'.'
-															<td>'.$data['montant_t'].' €</td>'.'
-															<td>'.$data['date_emi_t'].'</td>'.'
-															<td>'.$data['date_recouv_t'].'</td>'.'
-															<td>
-																	<button type="button" class="btn btn-default btn-circle">
-																			<i class="fa fa-pencil"></i>
-																	</button>
-																	<button type="button" class="btn btn-danger btn-circle">
-																			<i class="fa fa-times danger"></i>
-																	</button>
-															</td>'.'
-															</tr>';
-											}
-										?>
-									</tbody>
-								</table>
-								<!-- /.table-responsive -->
-								<?php include('../include/modal.php');?>
-								<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-										<div class="modal-dialog">
-												<div class="modal-content">
-														<div class="modal-header">
-																<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-																<h4 class="modal-title" id="myModalLabel">Ajouter une taxe</h4>
-														</div>
-														<div class="modal-body">
-															<form role="form" class="AVAST_PAM_nonloginform" method="post">
-																<label>Type</label>
-																<input class="form-control" name="type" id="type" required/>
-																<label>Montant</label>
-																<input class="form-control" name="montant" id="montant" required/>
-																<div class="modal-footer">
-																	<button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
-																	<button type="submit" name="submit" class="btn btn-primary">Ajouter</button>
-																</div>
-															</form>
-															<?php
-																if(isset($_POST['submit'])){
-																	 if(is_string($_POST['type']) && strlen($_POST['type'])<21 && preg_match('#^[0-9]+$#',$_POST['montant'])) {
-																		 $dateemi = date("Y-m-d H:i:s");
-																		 $daterecouv = date("Y-m-d H:i:s", mktime(23, 59, 59, date("m")+1 , date("d"), date("Y")));
-																		 echo '
-																		 <form id="formT" role="form" method="post" action="ajout.php">
-																		 	<input type="hidden" name="values" value="(type_t, montant_t, date_emi_t, date_recouv_t)"/>
-																			<input type="hidden" name="table" value="Taxe"/>
-																			<input type="hidden" name="red" value="taxe.php"/>
-																			<input type="hidden" name="a" value="'.$_POST['type'].'"/>
-																			<input type="hidden" name="b" value="'.$_POST['montant'].'"/>
-																			<input type="hidden" name="c" value="'.$dateemi.'"/>
-																			<input type="hidden" name="d" value="'.$daterecouv.'"/>
-																		</form>
-																		 <script>document.getElementById("formT").submit();</script>';
-																	 } else {
-																		 echo 'erreur';
-																		 $_POST = array();
-																	 }
-																}
-															?>
-													</div>
-												</div>
-												<!-- /.modal-content -->
-										</div>
-										<!-- /.modal-dialog -->
-								</div>
-								<?php include('../include/btn_add.php');?>  
-								<h4>
-									Montant de la TVA:
-									<?php
-										$result = $bdd->query("SELECT montant_tva FROM TVA where id_tva=1");
-										$data = $result->fetch();
-										echo '<strong>' . $data['montant_tva'].' %<strong/>';
-									?>
+                        $result = $bdd->query("SELECT * FROM Taxe");
+                        while($data = $result->fetch()){
+                            echo '<tr>
+                                        <td>'.$data['type_t'].'</td>'.'
+                                        <td>'.$data['montant_t'].' €</td>'.'
+                                        <td>'.$data['date_emi_t'].'</td>'.'
+                                        <td>'.$data['date_recouv_t'].'</td>'.'
+                                        <td>
+                                          <button type="button" class="btn btn-default btn-circle"><i class="fa fa-pencil"></i>
+                                          </button>
+                                          <button type="button" class="btn btn-danger btn-circle" data-toggle="modal" data-target="#modalDel" data-id="'. $data['id_t'] .'"  data-nomid="id_t" data-table="Taxe" data-red="taxe.php">
+                                            <i class="fa fa-times"></i>
+                                          </button>
+                                        </td>'.'
+                                        </tr>';
+                        }
+                    ?>
+                  </tbody>
+                </table>
+                <!-- /.table-responsive -->
+                <?php include('../include/modal.php');?>
+                <div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Ajouter une taxe</h4>
+                      </div>
+                      <div class="modal-body">
+                        <form role="form" class="AVAST_PAM_nonloginform" method="post">
+                          <label>Type</label>
+                          <input class="form-control" name="type" id="type" required/>
+                          <label>Montant</label>
+                          <input class="form-control" name="montant" id="montant" required/>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                            <button type="submit" name="submit" class="btn btn-primary">Ajouter</button>
+                          </div>
+                        </form>
+                        <?php
+                          if(isset($_POST['submit'])){
+                               if(is_string($_POST['type']) && strlen($_POST['type'])<21 && preg_match('#^[0-9]+$#',$_POST['montant'])) {
+                                   $dateemi = date("Y-m-d H:i:s");
+                                   $daterecouv = date("Y-m-d H:i:s", mktime(23, 59, 59, date("m")+1 , date("d"), date("Y")));
+                                   echo '
+                                   <form id="formT" role="form" method="post" action="ajout.php">
+                                      <input type="hidden" name="values" value="(type_t, montant_t, date_emi_t, date_recouv_t)"/>
+                                      <input type="hidden" name="table" value="Taxe"/>
+                                      <input type="hidden" name="red" value="taxe.php"/>
+                                      <input type="hidden" name="a" value="'.$_POST['type'].'"/>
+                                      <input type="hidden" name="b" value="'.$_POST['montant'].'"/>
+                                      <input type="hidden" name="c" value="'.$dateemi.'"/>
+                                      <input type="hidden" name="d" value="'.$daterecouv.'"/>
+                                  </form>
+                                   <script>document.getElementById("formT").submit();</script>';
+                               } else {
+                                   echo 'erreur';
+                                   $_POST = array();
+                               }
+                          }
+                      ?>
+                      </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
+                <?php include('../include/btn_add.php');?>
+                <h4>
+                  Montant de la TVA:
+                  <?php
+                      $result = $bdd->query("SELECT montant_tva FROM TVA where id_tva=1");
+                      $data = $result->fetch();
+                      echo '<strong>' . $data['montant_tva'].' %<strong/>';
+                  ?>
                 </h4>
               </div>
               <!-- /.panel-body -->
