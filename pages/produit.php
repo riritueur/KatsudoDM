7d5fa9500ad318f36b7b8cf6c8a5386b5fa97855
Richard Peres <riritueur@gmail.com>
2018-01-06 00:48:01 +0100

    Client, emp, four, prod delete

--- pages/produit.php
+++ pages/produit.php
@@ -3,28 +3,28 @@
 
 <head>
 
-    <?php include("../include/meta.php"); ?>
+  <?php include("../include/meta.php"); ?>
 
-    <title>Katsudo DM - Produits</title>
+  <title>Katsudo DM - Produits</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Bootstrap Core CSS -->
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- MetisMenu CSS -->
-    <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+  <!-- MetisMenu CSS -->
+  <link href="../vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
 
-    <!-- Custom CSS -->
-    <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
+  <!-- Custom CSS -->
+  <link href="../dist/css/sb-admin-2.css" rel="stylesheet">
 
-    <!-- Morris Charts CSS -->
-    <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
+  <!-- Morris Charts CSS -->
+  <link href="../vendor/morrisjs/morris.css" rel="stylesheet">
 
-    <!-- Custom Fonts -->
-    <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <!-- Custom Fonts -->
+  <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
@@ -47,68 +47,68 @@
         }
     ?>
 
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Produits</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Liste des produits
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                <thead>
-                                    <tr>
-                                        <th>Référence</th>
-                                        <th>Description</th>
-                                        <th>Image</th>
-                                        <th>Prix</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                    $result = $bdd->query("SELECT * FROM Produit");
-                                    while($data = $result->fetch()){
-                                        echo '<tr>
-                                                    <td>'.$data['ref_p'].'</td>'.'
-                                                    <td>'.$data['desc_p'].'</td>'.'
-                                                    <td><img height=50px src="'.$data['image_p'].'" alt='.$data['ref-p'].'/></td>'.'
-                                                    <td>'.$data['prix_ht'].' €</td>'.'
-                                                    <td>
-                                                        <button type="button" class="btn btn-default btn-circle">
-                                                            <i class="fa fa-pencil"></i>
-                                                        </button>
-                                                        <button type="button" class="btn btn-danger btn-circle">
-                                                            <i class="fa fa-times"></i>
-                                                        </button>
-                                                    </td>'.'
-                                                    </tr>';
-                                    }
-                                ?>
-                                <?php include('../include/modal.php');?>  
-                                </tbody>
-                            </table>
-                            <!-- /.table-responsive -->
-                          <?php include('../include/btn_add.php');?> 
-                        </div>
-											<img class="img-thumbnail" />
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
+      <div id="page-wrapper">
+        <div class="row">
+          <div class="col-lg-12">
+            <h1 class="page-header">Produits</h1>
+          </div>
+          <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                Liste des produits
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                    <tr>
+                      <th>Référence</th>
+                      <th>Description</th>
+                      <th>Image</th>
+                      <th>Prix</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                        $result = $bdd->query("SELECT * FROM Produit");
+                        while($data = $result->fetch()){
+                            echo '<tr>
+                                  <td>'.$data['ref_p'].'</td>'.'
+                                  <td>'.$data['desc_p'].'</td>'.'
+                                  <td><img height=50px src="'.$data['image_p'].'" alt='.$data['ref-p'].'/></td>'.'
+                                  <td>'.$data['prix_ht'].' €</td>'.'
+                                  <td>
+                                      <button type="button" class="btn btn-default btn-circle">
+                                          <i class="fa fa-pencil"></i>
+                                      </button>
+                                      <button type="button" class="btn btn-danger btn-circle" data-toggle="modal" data-target="#modalDel" data-id="'. $data['ref_p'] .'"  data-nomid="ref_p" data-table="Produit" data-red="produit.php">
+                                      <i class="fa fa-times"></i>
+                                      </button>
+                                  </td>'.'
+                                  </tr>';
+                        }
+                    ?>
+                    <?php include('../include/modal.php');?>
+                  </tbody>
+                </table>
+                <!-- /.table-responsive -->
+                <?php include('../include/btn_add.php');?>
+              </div>
+              <img class="img-thumbnail" />
+              <!-- /.panel-body -->
             </div>
             <!-- /.panel -->
           </div>
           <!-- /.col-lg-12 -->
-        <!-- /.row -->
+        </div>
+        <!-- /.panel -->
+      </div>
+      <!-- /.col-lg-12 -->
+      <!-- /.row -->
       <!-- /#page-wrapper -->
 
   </div>
