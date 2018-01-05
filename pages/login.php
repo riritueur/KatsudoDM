5522de34a306898070f6fba6a0559bd5f219a934
Richard Peres <riritueur@gmail.com>
2018-01-05 01:29:16 +0100

    login

--- pages/login.php
+++ pages/login.php
@@ -36,26 +36,92 @@
             <h3 class="panel-title">Connexion au système</h3>
           </div>
           <div class="panel-body">
-            <form role="form">
+            <form role="form" method="post" novalidate>
               <fieldset>
                 <div class="form-group">
                   <input class="form-control" placeholder="admin" name="id" type="text" autofocus>
                 </div>
                 <div class="form-group">
-                  <input class="form-control" placeholder="password" name="password" type="password" value="">
+                  <input class="form-control" placeholder="password" name="pass" type="password" value="">
                 </div>
                 <!-- Change this to a button or input when using this as a form -->
-                <button href="index.php" class="btn btn-lg btn-success btn-block" type="submit">Se connecter</button>
+                <button href="index.php" class="btn btn-lg btn-success btn-block" type="submit" onclick="auth()">Se connecter</button>
               </fieldset>
             </form>
           </div>
         </div>
       </div>
+      <?php
+          if(empty($_POST['id']) && empty($_POST['id'])) {
+            echo '
+              <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog">
+                  <div class="modal-content">
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                          <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                      </div>
+                      <div class="modal-body">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                  </div>
+                  <!-- /.modal-content -->
+              </div>
+              <!-- /.modal-dialog -->
+          </div> ';
+          }
+          else {
+            try{
+                $bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u178917848_katsu;charset=utf8', 'u178917848_kuser', 'password');
+            }
+            catch (Exception $e)
+            {
+              die('Erreur : ' . $e->getMessage());
+            }
+
+            $result = $bdd->query("SELECT * FROM Utilisateur");
+            $sign = false;
+            while($data = $result->fetch()){
+              if($data['id_user'] == $_POST['id'] && $data['pass_user'] == $_POST['pass'])
+                $sign = true;
+            }
+            if($sign) {
+              // TODO COOKIE
+            }
+            else {
+              echo '
+              <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                            <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                                        </div>
+                                        <div class="modal-body">
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-primary">Save changes</button>
+                                        </div>
+                                    </div>
+                                    <!-- /.modal-content -->
+                                </div>
+                                <!-- /.modal-dialog -->
+                            </div> ';
+            }
+          }
+        
+    ?>
     </div>
+    
   </div>
 
   <?php include('../include/scripts.php'); ?>
-
 </body>
 
 </html>
