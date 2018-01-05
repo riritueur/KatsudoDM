0abb876ca5d2b1288c63a083049c1ea0dad55c0f
Richard Peres <riritueur@gmail.com>
2018-01-05 01:29:52 +0100

    tabulation

--- pages/fournisseur.php
+++ pages/fournisseur.php
@@ -51,69 +51,69 @@
 					}
 				?>
 
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Fournisseurs</h1>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Liste des fournisseurs
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                <thead>
-                                    <tr>
-                                        <th>Nom</th>
-                                        <th>Prénom</th>
-                                        <th>Adresse</th>
-                                        <th>Téléphone</th>
-                                        <th>E-mail</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php
-                                    $result = $bdd->query("SELECT * FROM Fournisseur");
-                                    while($data = $result->fetch()){
-                                        echo '<tr>
-                                                    <td>'.$data['nom_f'].'</td>'.'
-                                                    <td>'.$data['prenom_f'].'</td>'.'
-                                                    <td>'.$data['adresse_f'].'</td>'.'
-                                                    <td>'.$data['tel_f'].'</td>'.'
-                                                    <td>'.$data['email_f'].'</td>'.'
-                                                    <td>
-                                                        <button type="button" class="btn btn-default btn-circle">
-                                                            <i class="fa fa-pencil"></i>
-                                                        </button>
-                                                        <button type="button" class="btn btn-danger btn-circle">
-                                                            <i class="fa fa-times"></i>
-                                                        </button>
-                                                    </td>'.'
-                                                    </tr>';
-                                    }
-                                ?>
-                                </tbody>
-                            </table>
-                            <!-- /.table-responsive -->
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
-                    <!-- /.panel -->
-                </div>
-                <!-- /.col-lg-12 -->
+      <div id="page-wrapper">
+        <div class="row">
+          <div class="col-lg-12">
+            <h1 class="page-header">Fournisseurs</h1>
+          </div>
+          <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="panel panel-default">
+              <div class="panel-heading">
+                Liste des fournisseurs
+              </div>
+              <!-- /.panel-heading -->
+              <div class="panel-body">
+                <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                  <thead>
+                    <tr>
+                      <th>Nom</th>
+                      <th>Prénom</th>
+                      <th>Adresse</th>
+                      <th>Téléphone</th>
+                      <th>E-mail</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                      $result = $bdd->query("SELECT * FROM Fournisseur");
+                      while($data = $result->fetch()){
+                          echo '<tr>
+                                      <td>'.$data['nom_f'].'</td>'.'
+                                      <td>'.$data['prenom_f'].'</td>'.'
+                                      <td>'.$data['adresse_f'].'</td>'.'
+                                      <td>'.$data['tel_f'].'</td>'.'
+                                      <td>'.$data['email_f'].'</td>'.'
+                                      <td>
+                                          <button type="button" class="btn btn-default btn-circle">
+                                              <i class="fa fa-pencil"></i>
+                                          </button>
+                                          <button type="button" class="btn btn-danger btn-circle">
+                                              <i class="fa fa-times"></i>
+                                          </button>
+                                      </td>'.'
+                                      </tr>';
+                      }
+                  ?>
+                  </tbody>
+                </table>
+                <!-- /.table-responsive -->
+              </div>
+              <!-- /.panel-body -->
             </div>
             <!-- /.panel -->
           </div>
           <!-- /.col-lg-12 -->
         </div>
-        <!-- /.row -->
-      <!-- /#page-wrapper -->
+        <!-- /.panel -->
+      </div>
+      <!-- /.col-lg-12 -->
+  </div>
+  <!-- /.row -->
+  <!-- /#page-wrapper -->
 
   <?php include('../include/scripts'); ?>
 
