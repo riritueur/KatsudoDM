232af031a135202223b094c7d0b93db74d7537db
Richard Peres <riritueur@gmail.com>
2018-01-05 02:13:19 +0100

    login + session

--- index.php
+++ index.php
@@ -2,20 +2,150 @@
 <html lang="fr">
 
 <head>
-  <?php include('../include/meta.php'); ?>
-  <title>Katsudo DM</title>
+  <?php include('include/meta.php'); ?>
+  <title>Katsudo DM - Login</title>
 
-  <meta http-equiv="refresh" content="0;url=pages/index.php">
-  
-  <script language="javascript">
-    window.location.href = "pages/index.php"
-  </script>
+  <!-- Bootstrap Core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- MetisMenu CSS -->
+  <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link href="dist/css/sb-admin-2.css" rel="stylesheet">
+
+  <!-- Custom Fonts -->
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
 
 <body>
-  Redirection en cours, si vous êtes sur cette page 
-  depuis plus de 5 secondes veuillez cliquer
-  <a href="pages/index.php">ici</a>
+  
+  <?php
+  
+  session_start();
+  
+  if(!empty($_COOKIE['login'])) {
+    header('Location: http://katsudodm.richard-peres.xyz/pages/');
+    exit;
+  }
+    
+  if(!empty($_POST['id']) && !empty($_POST['id'])) {
+      $signin = false;
+      
+      try{
+          $bdd = new PDO('mysql:host=mysql.hostinger.fr;dbname=u178917848_katsu;charset=utf8', 'u178917848_kuser', 'password');
+      }
+      catch (Exception $e)
+      {
+        die('Erreur : ' . $e->getMessage());
+      }
+      $result = $bdd->query("SELECT * FROM Utilisateur");
+      while($data = $result->fetch()){
+        if($data['id_user'] == $_POST['id'] && $data['pass_user'] == $_POST['pass'])
+          $sign = true;
+      }
+      
+      if($signin) {
+  
+        setcookie("login",$_POST['id'],time()+1800); 
+        
+        header('Location: http://katsudodm.richard-peres.xyz/pages/index.php');
+        exit;
+      }
+      else {
+        echo '
+            <div class="container">
+              <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                  <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                      <h3 class="panel-title text-center"><strong>Connexion au système</strong></h3>
+                    </div>
+                    <div class="panel-body">
+                      <form role="form" method="post">
+                      <br/>
+                        <fieldset>
+                          <div class="form-group input-group has-error">
+                            <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                            <input class="form-control" placeholder="admin" name="id" type="text" autofocus>
+                          </div>
+                          
+                          <br/>
+
+                          <div class="form-group input-group has-error">
+                            <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                            <input class="form-control" placeholder="password" name="pass" type="password" value="">
+                          </div>
+                          
+                          <div class="text-danger text-center">
+                          <br/>
+                          <i class="fa fa-times"></i><strong>
+                            Identifiant ou mot de passe incorrect. Veuillez réessayer.<br/><br/>
+                            </strong>
+                          </div>
+
+                          <!-- Change this to a button or input when using this as a form -->
+                          <button href="index.php" class="btn btn-lg btn-success btn-block" type="submit">Se connecter</button>
+                        </fieldset>
+                      </form>
+                    </div>
+                  </div>
+                </div>';
+      }
+    } else {
+    
+    echo '
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+              <div class="login-panel panel panel-default">
+                <div class="panel-heading">
+                  <h3 class="panel-title text-center"><strong>Connexion au système</strong></h3>
+                </div>
+                <div class="panel-body">
+                  <form role="form" method="post">
+                  <br/>
+                    <fieldset>
+                      <div class="form-group input-group">
+                        <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                        <input class="form-control" placeholder="admin" name="id" type="text" autofocus>
+                      </div>
+                      
+                      <br/>
+                      
+                      <div class="form-group input-group">
+                        <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                        <input class="form-control" placeholder="password" name="pass" type="password" value="">
+                      </div>
+                      <br/>
+                      <!-- Change this to a button or input when using this as a form -->
+                      <button href="index.php" class="btn btn-lg btn-success btn-block" type="submit">Se connecter</button>
+                    </fieldset>
+                  </form>
+                </div>
+              </div>
+            </div>';
+  
+  }
+  
+  
+      
+          
+        
+    echo '
+    </div>
+    
+  </div>';
+  ?>
+  <?php include('include/scripts.php'); ?>
 </body>
 
 </html>
---
+++ index_old.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <?php include('../include/meta.php'); ?>
+  <title>Katsudo DM</title>
+
+  <meta http-equiv="refresh" content="0;url=pages/index.php">
+  
+  <script language="javascript">
+    window.location.href = "pages/index.php"
+  </script>
+</head>
+
+<body>
+  Redirection en cours, si vous êtes sur cette page 
+  depuis plus de 5 secondes veuillez cliquer
+  <a href="pages/index.php">ici</a>
+</body>
+
+</html>
