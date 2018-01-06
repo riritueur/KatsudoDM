0a04f07142eede50ba2727835ce760ab5605a2a3
Francois Rigaut <francois.rigaut@etu.unice.fr>
2018-01-06 18:44:47 +0100

    Ajout fournisseur

--- pages/facture-fournisseur.php
+++ pages/facture-fournisseur.php
@@ -116,7 +116,7 @@
                                                 <button type="button" class="btn btn-default btn-circle">
                                                         <i class="fa fa-pencil"></i>
                                                 </button>
-                                                <button type="button" class="btn btn-danger btn-circle" data-toggle="modal" data-target="#modalDel" data-id="'. $data['id_fac_f'] .'"  data-nomid="id_fac_f" data-table="Facture_Fournisseur" data-red="employe.php">
+                                                <button type="button" class="btn btn-danger btn-circle" data-toggle="modal" data-target="#modalDel" data-id="'. $data['id_fac_f'] .'"  data-nomid="id_fac_f" data-table="Facture_Fournisseur" data-red="facture-fournisseur.php">
                                                 <i class="fa fa-times"></i>
                                                 </button>
                                                 <button type="button" class="btn btn-default btn-circle">
@@ -131,6 +131,85 @@
                 </table>
                 <!-- /.table-responsive -->
                 <?php include('../include/btn_add.php');?>
+								<?php
+									$resultfournlist = $bdd->query('SELECT id_f, nom_f, prenom_f FROM Fournisseur');
+								?>
+								<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        <h4 class="modal-title" id="myModalLabel">Ajouter une facture de fournisseur</h4>
+                      </div>
+                      <div class="modal-body">
+                        <form role="form" class="AVAST_PAM_nonloginform" method="post">
+                          <label>Référence</label>
+                          <input class="form-control" name="ref" id="ref" required/>
+                          <label>Fournisseur</label>
+                          <select class="form-control" name="fourn" id="fourn" required>
+														<?php
+															while($datafournlist = $resultfournlist->fetch()){
+																echo '<option value="'.$datafournlist['id_f'].'">'.$datafournlist['nom_f'].' '.$datafournlist['prenom_f'].'</option>';
+															}
+														?>
+													</select>
+													<label>Description</label>
+                          <input class="form-control" name="desc" id="desc" required/>
+													<label>Prix HT</label>
+                          <input class="form-control" name="prixht" id="prixht" required/>
+													<label>État</label>
+                          <div class="radio">
+														<label>
+															<input type="radio" name="etat" id="nopaye" value="0" checked/>Non-payée
+														</label>
+													</div>
+													<div class="radio">
+														<label>
+															<input type="radio" name="etat" id="paye" value="1"/>Payée
+														</label>
+													</div>
+                          <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                            <button type="submit" name="submit" class="btn btn-primary">Ajouter</button>
+                          </div>
+                        </form>
+                        <?php
+                          if(isset($_POST['submit'])){
+                               if(strlen($_POST['ref'])<21 && strlen($_POST['desc'])<201 && preg_match('#^[0-9]+$#',$_POST['prixht']) && ($_POST['etat'] == "1" || $_POST['etat'] == "0")) {
+																	$dateemi = date("Y-m-d H:i:s");
+																	$daterecouv = date("Y-m-d H:i:s", mktime(23, 59, 59, date("m")+1 , date("d"), date("Y")));
+																	$resulttva = $bdd->query("SELECT montant_tva FROM TVA WHERE id_tva=1");
+																	$valeurtva = $resulttva->fetch();
+																	$montanttva = round((intval($_POST['prixht'])*intval($valeurtva['montant_tva']))/100);
+																	$prixttc = $_POST['prixht'] + $montanttva;
+																	echo '
+																	<form id="formT" role="form" method="post" action="ajout.php">
+																		<input type="hidden" name="values" value="(id_f, ref_fac_f, desc_fac_f, date_emi_fac_f, date_rec_fac_f, montant_ht_fac_f, montant_tva_fac_f, montant_ttc_fac_f, paiement_fac_f)"/>
+																		<input type="hidden" name="table" value="Facture_Fournisseur"/>
+																		<input type="hidden" name="red" value="facture-fournisseur.php"/>
+																		<input type="hidden" name="a" value="'.$_POST['fourn'].'"/>
+																		<input type="hidden" name="b" value="'.$_POST['ref'].'"/>
+																		<input type="hidden" name="c" value="'.$_POST['desc'].'"/>
+																		<input type="hidden" name="d" value="'.$dateemi.'"/>
+																		<input type="hidden" name="e" value="'.$daterecouv.'"/>
+																		<input type="hidden" name="f" value="'.$_POST['prixht'].'"/>
+																		<input type="hidden" name="g" value="'.$montanttva.'"/>
+																		<input type="hidden" name="h" value="'.$prixttc.'"/>
+																		<input type="hidden" name="i" value="'.$_POST['etat'].'"/>
+																	</form>
+																	 <script>document.getElementById("formT").submit();</script>';
+                               } else {
+                                   echo 'erreur';
+                                   $_POST = array();
+                               }
+                          }
+                      ?>
+                      </div>
+                    </div>
+                    <!-- /.modal-content -->
+                  </div>
+                  <!-- /.modal-dialog -->
+                </div>
               </div>
             </div>
           </div>
